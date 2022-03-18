27097eef652558d909b671b88ab3a7d7b9d5d481
mitali-m <mitali.ntu@gmail.com>
2022-03-18 15:00:07 +0800

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
 
-function App() {
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, CircularProgress , Link, CssBaseline, Container, AppBar, Toolbar, TextField, IconButton, Input, Box, Grid, Paper, Avatar, Checkbox, FormControl, InputLabel, Select, MenuItem, ListItemText, Button } from '@material-ui/core';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import SearchIcon from '@material-ui/icons/Search';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import TrendingDownIcon from '@material-ui/icons/TrendingDown';
+import grey from '@material-ui/core/colors/grey';
 
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:5000');
+
+function App() {
+  var searchQuery = React.useRef()
   socket.on('connect', function() {
     socket.emit('join', {data: 'Client connected!', cid: socket.id});
-});
+  });
   const handleSearchClick = (event) => {
     var searchq = searchQuery.current.value;
     if(searchq==="") return;
@@ -24,34 +39,15 @@ function App() {
     console.log("sent");
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="A">
       <Grid container item xs={10} justify='center'> 
-
-            <Grid container item sm={11} xs={10} justify='center'> 
-              <TextField inputRef={searchQuery} variant="outlined" label="Search for" color="secondary" fullWidth/>
-            </Grid>
-
-            <Grid container item sm={1} xs={2} justify='center'> 
-              <IconButton type="submit" onClick={handleSearchClick} className={classes.iconButton} aria-label="search">
-                <SearchIcon />
-              </IconButton>
-            </Grid>
-
-          </Grid>
+        <Grid container item sm={11} xs={10} justify='center'> 
+          <TextField inputRef={searchQuery} variant="outlined" label="Search for" color="secondary" fullWidth/>
+        </Grid>
+        <Grid container item sm={1} xs={2} justify='center'> 
+          <IconButton type="submit" onClick={handleSearchClick} aria-label="search"> submit </IconButton>
+        </Grid>
+      </Grid>
     </div>
   );
 }
